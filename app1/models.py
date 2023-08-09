42d845550b6310ab3b93f2022ac5b6e009b5ec99
arun <arunkvk93@gmail.com>
2023-08-09 22:13:07 +0530

    add database and table

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class tbl_hospital(models.Model):
+    Emp_id=models.IntegerField()
+    Doctor=models.CharField(max_length=100)
+    Specialization=models.CharField(max_length=100)
+    Salary=models.IntegerField()
+    Experience=models.CharField(max_length=100)
+    Age=models.IntegerField()
+    Gender=models.CharField(max_length=100)
+    class Meta:
+        db_table="tbl_hospital"
--- hospital/urls.py
+++ hospital/urls.py
@@ -20,4 +20,5 @@ from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index),
+    path("admin/",views.admin),
 ]
